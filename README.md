8b44298e971d8cd7bf4520b74b5ac45ad601a962
Tomcariello <tomcariello@gmail.com>
2016-08-15 07:32:30 -0400

    wins & losses processing, traditional graphics cycling correctly, made current to-do list

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # week-3-game
+
+8/15/2016 To Do List
+Do not count a letter as a guess if it has already been selected
+Do not show a letter in the "letters guessed" box if it has already been selected
+Set tradition/dark toggle
+set easy/heard (or delete/hide that option)
+Add reset button
--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -6,12 +6,14 @@
 var wordBeingPlayed = "";
 var currentWord;
 var guesses = 0;
+var totalMatchedGuesses = 0;
+var lettersGuessed = [];
+var playerWins = 0;
+var playerLosses = 0;
 
 
 function initialize() {
 	var opponentChoice;
-	var playerWins = 0;
-	var playerLosses = 0;
 	var choiceArray = ["archery","athletics","badminton","baseball","basketball","basque pelota","boxing","canoeing","cricket","croquet","cycling","diving","equestrian","fencing","field hockey","football","golf","gymnastics","handball","jeu de paume","judo","kayaking","lacrosse","pentathlon","polo","racquets","rhythmic gymnastics","roque","rowing","rugby","sailing","shooting","softball","swimming","synchronized swimming","table tennis","taekwondo","tennis","trampoline","triathlon","tug of war","volleyball","water motorsports","water polo","weightlifting","wrestling"];
 
 	// 	Randomly select word for game from array
@@ -49,15 +51,16 @@ document.onkeydown = function(event) {
 		// console.log("Word is " + currentWord + " letterPlayed is " + letterPlayed + " currentWord is " + currentWord);
 		if (letterPlayed == currentWord[k]) {
 			tempWordBeingPlayed += letterPlayed;
-			console.log("Match! tempWordBeingPlayed is " + tempWordBeingPlayed);
+			// console.log("Match! tempWordBeingPlayed is " + tempWordBeingPlayed);
 			matchedGuess = true;
+			totalMatchedGuesses++;
 		}
 		 else if (wordBeingPlayed[k] != "_") {
 			tempWordBeingPlayed += wordBeingPlayed[k];
-			console.log("Already selected! tempWordBeingPlayed is " + tempWordBeingPlayed);
+			// console.log("Already selected! tempWordBeingPlayed is " + tempWordBeingPlayed);
 		} else {
 			tempWordBeingPlayed += "_";
-			console.log("Blank space! tempWordBeingPlayed is " + tempWordBeingPlayed);
+			// console.log("Blank space! tempWordBeingPlayed is " + tempWordBeingPlayed);
 		}
 		document.getElementById("wordstatus").innerHTML	= tempWordBeingPlayed;
 
@@ -66,23 +69,76 @@ document.onkeydown = function(event) {
 
 	// 		Display letter in "already used" bin
 	if (matchedGuess == true) {
+		printLetters();
+		updateBoardImage();
 		wordComplete();
 	}
 	
 	if (matchedGuess == false) {
 		guesses++;
-		document.getElementById("usedletters").innerHTML = guesses;
-		loseGame();	
+		printLetters();
+		updateBoardImage();	
 	}
 	
 }
 
 function wordComplete() {
-
+	for (i=0; i < wordBeingPlayed.length; i++) {
+		if (wordBeingPlayed[i] == "_") {
+			console.log(wordBeingPlayed[i]);
+			return;
+		} 
+	}
+	playerWins++;
+	updateScoreboard("win");
 }
 
-function loseGame() {
-	
+
+//update graphic on board
+function updateBoardImage() {
+	//If gametype is traditional && easy
+	if (guesses == 1) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/1.png";
+	} else if (guesses == 2) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/2.png";
+	} else if (guesses == 3) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/3.png";
+	} else if (guesses == 4) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/4.png";
+	} else if (guesses == 5) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/5.png";
+	} else if (guesses == 6) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/6.png";
+	} else if (guesses == 7) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/7.png";
+	} else if (guesses == 8) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/8.png";
+	} else if (guesses == 9) {
+		document.getElementById("imagedisplayed").src="assets/images/traditionaleasy/9.png";
+		playerLosses++;
+		updateScoreboard("lose");
+	}
+}
+
+function updateScoreboard(gameStatus) {
+	if (gameStatus == "win") {
+		alert("You win!");
+	} else if (gameStatus == "lose") {
+		alert("You lose!");
+	}
+
+	document.getElementById("scoreboard").innerHTML = "<p>Player wins: " + playerWins + "</p><p>Player Losses:" + playerLosses + "</p>";
+}
+
+function printLetters() {
+	lettersGuessed.push(letterPlayed);
+	for (i = 0; i < lettersGuessed.length; i++) {
+		if (i == 0) {
+			document.getElementById("usedletters").innerHTML = lettersGuessed[i];	
+		} else {
+		document.getElementById("usedletters").innerHTML += ", " + lettersGuessed[i];
+	}
+	}
 }
 
 
